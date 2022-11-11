787b331456d2a469c1ab04b6f2f015c82ac42ee9
Robin <24046@ma-web.nl>
2022-11-11 15:37:16 +0100

    Video 12 done
    Video 12 done,
    Wasn't able to finish the project Intend to do so soon

--- myskillz/src/App/App.js
+++ myskillz/src/App/App.js
@@ -1,14 +1,17 @@
 import Question from "../Question/Question";
 import './App.css';
+import Intro from "../Intro/Intro";
 import React, { Component }  from 'react';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-      <BrowserRouter>
-
-        <Question />
-      </BrowserRouter>
+      <>
+      <Routes>
+          <Route  path="/question" element={<Question />}></Route>
+          <Route  path="/" element={<Intro />}></Route>
+      </Routes>
+      </>
   );
 }
 
---
+++ myskillz/src/Intro/Intro.js
@@ -0,0 +1,14 @@
+import "./Intro.css";
+import React, { Component }  from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+const Intro = () => {
+    return(
+        <>
+            <h1>Hello</h1>
+
+        </>
+    )
+}
+
+export default Intro;
